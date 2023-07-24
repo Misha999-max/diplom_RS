aa9b3d3075a6faf7cce04d0d80c54bc4fb038ccc
__BEAST__ <mihailkalacev9@gmail.com>
2023-07-24 13:00:59 +0300

    create auth back

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -2,16 +2,16 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
 
-router.get("/", async (req, res) => {
-  try {
-    const allUser = await User.find();
-    console.log(allUser);
-    res.status(200).send({ list: allUser });
-  } catch (error) {
-    res.status(500).json({
-      message: "на сервере произошла ошибка попробуйте позже",
-    });
-  }
-});
+// router.get("/", async (req, res) => {
+//   try {
+//     const allUser = await User.find();
+//     console.log(allUser);
+//     res.status(200).send({ list: allUser });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "на сервере произошла ошибка попробуйте позже",
+//     });
+//   }
+// });
 
 module.exports = router;
